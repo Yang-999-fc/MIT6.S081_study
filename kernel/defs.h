56140d0280e6332e4ee432327988497c7265a200
Yang-999-fc <57994308+Yang-999-fc@users.noreply.github.com>
2024-05-16 19:55:33 +0800

    Add files via upload

--- kernel/defs.h
+++ kernel/defs.h
@@ -80,7 +80,6 @@ int             pipewrite(struct pipe*, uint64, int);
 void            printf(char*, ...);
 void            panic(char*) __attribute__((noreturn));
 void            printfinit(void);
-void			backtrace(void);
 
 // proc.c
 int             cpuid(void);
@@ -146,8 +145,6 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
-int             sigalarm(int n, void(*fn)(void));
-int             sigreturn();
 
 // uart.c
 void            uartinit(void);
